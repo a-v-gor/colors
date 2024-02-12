d569e7a63248751f97079a4fb2367ff7d65a1aa2
Alexey Gorbenko <a.v.gor@mail.ru>
2024-02-12 19:32:44 +0300

    feat: add save game to local storage

--- script.js
+++ script.js
@@ -2,11 +2,12 @@ import { colors } from './colors.js';
 
 let wrapper;
 let colorsForChange = [];
+let colorsArr = JSON.parse(localStorage.getItem('colors')) || [].concat(colors);
 
 function addColors(){
-  for (let i = 0; i < colors.length; i +=1) {
+  for (let i = 0; i < colorsArr.length; i +=1) {
     const div = document.getElementById(i);
-    const data = colors[i];
+    const data = colorsArr[i];
     div.innerText = data.name.toUpperCase();
     div.style.backgroundColor = '#' + data.color;
   };
@@ -32,14 +33,26 @@ function startProgram() {
 
 function changeColors(event) {
   if (event.target.className === 'color-block') {
+    const infoBlock = document.querySelector('.info');
     if (!colorsForChange.length) {
       colorsForChange.push(event.target);
+      console.log(event.target.textContent);
+      infoBlock.innerText = event.target.textContent;
     } else {
       [colorsForChange[0].textContent, event.target.textContent] = [event.target.textContent, colorsForChange[0].textContent];
       [colorsForChange[0].style.backgroundColor, event.target.style.backgroundColor] = [event.target.style.backgroundColor, colorsForChange[0].style.backgroundColor];
+      console.log();
+      [colorsArr[colorsForChange[0].id], colorsArr[event.target.id]] = [colorsArr[event.target.id], colorsArr[colorsForChange[0].id]];
       colorsForChange.length = 0;
+      infoBlock.innerText = 'Info';
     }
   }
 }
 
+function saveGameData() {
+  console.log(colorsArr);
+  localStorage.setItem('colors', JSON.stringify(colorsArr))
+}
+
 document.addEventListener('DOMContentLoaded', startProgram);
+window.addEventListener('beforeunload', saveGameData);
